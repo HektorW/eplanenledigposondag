9fb40f51a38245ad4ce08645b801b9a52f29f346
Hektor Wallin <hektor.wallin@eletive.com>
2025-05-16 20:16:00 +0200

    Update search term when scraping
    Something seems to have changed on the page that made
    search results behave differently

--- src/lib/server/scrapeBlazor/step2SearchForSorgenfri.ts
+++ src/lib/server/scrapeBlazor/step2SearchForSorgenfri.ts
@@ -39,7 +39,7 @@ export async function searchAndClickLabels(page: Page, retryCount = 0): Promise<
 
 		// enough to only get sorgenfri hits, avoids detched
 		// avoids additional searches which detaches found labels
-		const searchTerm = 'sor ma';
+		const searchTerm = 'sor man';
 		await searchInput.type(searchTerm, { delay: 50 });
 		logger.debug('Typed search term:', searchTerm);
 		await searchInput.press('Enter');
